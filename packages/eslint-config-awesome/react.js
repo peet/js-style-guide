8dc92a69709e095604840b81f9f364f71243f41e
Peet <peter.goddard@contactpartners.com>
2016-03-10 10:52:09 +0000

    some react rule tweaks

--- packages/eslint-config-awesome/react.js
+++ packages/eslint-config-awesome/react.js
@@ -7,13 +7,14 @@ module.exports = {
   'extends': 'awesome/base',
   'rules': {
     'react/jsx-boolean-value': 2,
-    'react/jsx-closing-bracket-location': [2, 'after-props'],
+    'react/jsx-closing-bracket-location': [2, 'line-aligned'],
     'react/jsx-curly-spacing': 2,
     'react/jsx-indent': [1, 2],
     'react/jsx-indent-props': [1, 2],
     'react/jsx-no-bind': 1,
     'react/jsx-no-undef': 2,
     'react/jsx-pascal-case': 2,
+    'react/jsx-space-before-closing': [2, 'always'],
     'react/jsx-uses-react': 2,
     'react/jsx-uses-vars': 2,
     'react/no-deprecated': [1, { 'react': '0.14.0' }],
@@ -22,9 +23,21 @@ module.exports = {
     'react/no-is-mounted': 2,
     'react/no-multi-comp': [2, { 'ignoreStateless': true }],
     'react/no-unknown-property': 2,
+//    'react/prefer-stateless-function': 2,
     'react/react-in-jsx-scope': 2,
     'react/self-closing-comp': 2,
-    'react/wrap-multilines': [2, { 'declaration': true, 'assignment': false, 'return': true }]
+    'react/sort-comp': [1, {
+      'order': [
+        'static-methods',
+        'lifecycle',
+        '/^on.+$/',
+        '/^(get|set)(?!(InitialState$|DefaultProps$|ChildContext$)).+$/',
+        'everything-else',
+        '/^render.+$/',
+        'render'
+      ]
+    }],
+    'react/wrap-multilines': 2
   },
   'plugins': [
     'react'
